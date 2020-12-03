1161fc1b1a689f574eb5ad2987311c751d0321ed
ShiningShar <422620530@qq.com>
2020-12-03 10:35:06 +0800

    update img2area

--- area.py
+++ area.py
@@ -16,60 +16,62 @@ import util
 
 
 def draw_pixel(img, x, f):
-    for cnt in range(len(x) - 1):
-        cv.line(img, (x[cnt], f[cnt]), (x[cnt + 1], f[cnt + 1]), 0, thickness=1)
+    for tmp in range(len(x) - 1):
+        cv.line(img, (x[tmp], f[tmp]), (x[tmp + 1], f[tmp + 1]), 0, thickness=1)
     cv.line(img, (x[-1], f[-1]), (x[0], f[0]), 0, thickness=1)
 
 
 def process_pixel(x_new, f_new):
-    arr = np.full((256, 256), 255, np.uint8)
-    draw_pixel(arr, x_new, f_new)
-    img2 = np.flip(arr, axis=0)
+    background_arr = np.full((256, 256), 255, np.uint8)
+    draw_pixel(background_arr, x_new, f_new)
+    img2 = np.flip(background_arr, axis=0)
     return img2
 
 
 # 简单归一化，x和f均是根据全局变量的最大值进行放缩，而不是局部最大值
-def normalization(x, x_max):
+def normalization(x, data_max):
     if x_max.any() == 0:
-        return x_max, x
-    alpha = 256 / x_max
+        return data_max, x
+    alpha = 253 / data_max
     x = x * alpha
-    return x_max, x
+    return data_max, x
 
 
-def getArea(data):
+def getArea(org_data):
     # data是最原始的示功图载荷位移数据
-    cnt = 0
+    num = 0
     up_index = 0
     down_index = 0
     s = 0
     # 此处默认示功图数据点数为200，如果为144点数据需要修改常量198为142
-    while cnt < 198:
-        if cnt % 2 == 0:
-            p1 = data[up_index]
-            p2 = data[up_index + 1]
-            p3 = data[199 - down_index]
+    while num < 198:
+        if num % 2 == 0:
+            p1 = org_data[up_index]
+            p2 = org_data[up_index + 1]
+            p3 = org_data[199 - down_index]
             a = float(math.sqrt((p2[0] - p3[0]) * (p2[0] - p3[0]) + (p2[1] - p3[1]) * (p2[1] - p3[1])))
             b = float(math.sqrt((p1[0] - p3[0]) * (p1[0] - p3[0]) + (p1[1] - p3[1]) * (p1[1] - p3[1])))
             c = float(math.sqrt((p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1])))
-            l = (a + b + c) / 2
-            s = s + (l * (l - a) * (l - b) * (l - c)) ** 0.5
+            half_of_perimeter = (a + b + c) / 2
+            s = s + (half_of_perimeter * (half_of_perimeter - a) * (half_of_perimeter - b) * (
+                        half_of_perimeter - c)) ** 0.5
             up_index += 1
         else:
-            p1 = data[199 - down_index]
-            p2 = data[199 - down_index - 1]
-            p3 = data[up_index]
+            p1 = org_data[199 - down_index]
+            p2 = org_data[199 - down_index - 1]
+            p3 = org_data[up_index]
             a = float(math.sqrt((p2[0] - p3[0]) * (p2[0] - p3[0]) + (p2[1] - p3[1]) * (p2[1] - p3[1])))
             b = float(math.sqrt((p1[0] - p3[0]) * (p1[0] - p3[0]) + (p1[1] - p3[1]) * (p1[1] - p3[1])))
             c = float(math.sqrt((p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1])))
-            l = (a + b + c) / 2
-            s = s + (l * (l - a) * (l - b) * (l - c)) ** 0.5
+            half_of_perimeter = (a + b + c) / 2
+            s = s + (half_of_perimeter * (half_of_perimeter - a) * (half_of_perimeter - b) * (
+                        half_of_perimeter - c)) ** 0.5
             down_index += 1
-        cnt += 1
+        num += 1
     return s
 
 
-class img2area():
+class Img2Area:
     def __init__(self):
         self.dict = {
             'FD-1': (5.272, 119.84),
@@ -82,7 +84,7 @@ class img2area():
             'FD-8': (5.998, 78.41),
         }
 
-    def countArea(self, img_name, img_arr):
+    def count_area(self, img_name, img_arr):
         list_up = []
         list_down = []
         for col in range(len(img_arr)):
@@ -100,32 +102,32 @@ class img2area():
         tmp = tmp2 - tmp1
         s = tmp.sum()
         data_max = self.dict.get(img_name[0:4])
-        mul = (256 / data_max[0]) * (256 / data_max[1])
+        mul = (253 / data_max[0]) * (253 / data_max[1])
         return s / mul
 
     def count_area_ratio(self, img_name1, img_name2):
-        s1 = self.count_area_by_name(img_name1)
-        s2 = self.count_area_by_name(img_name2)
-        return float(s1 / s2)
+        s_1 = self.count_area_by_name(img_name1)
+        s_2 = self.count_area_by_name(img_name2)
+        return float(s_1 / s_2)
 
     def count_area_by_name(self, img_name):
         loadfile = load.LoadData('D:\\data\\name_point\\image_data_map.csv')
-        data, dict_index = loadfile.get_pixel_data()
+        pixel_data, dict_index = loadfile.get_pixel_data()
         index = dict_index.get(img_name)
         x, f = load.get_pixel_by_index(index, data)
-        arr = process_pixel(x, f)
-        s = self.count_area(arr)
+        img_arr = process_pixel(x, f)
+        s = self.count_area(img_name, img_arr)
         return s
 
 
 if __name__ == '__main__':
     # ratio = img2area().count_area_ratio('FD-1-12968.png', 'FD-1-12867.png')
     # print(ratio)
-    root = 'D:\data'
-    data_path = os.path.join(root, 'orgdata')
+    root = 'D:\\data'
+    data_path = os.path.join(root, 'orgData')
     listdir = os.listdir(data_path)
     for loc in listdir:
-        if loc != 'FD-1.csv':
+        if loc != 'FD-2.csv':
             continue
         data = pd.read_csv(os.path.join(data_path, loc), encoding='GBK', usecols=['位移', '载荷'])
         data = data.values
@@ -148,8 +150,7 @@ if __name__ == '__main__':
                 continue
             data_tmp = np.array(data_idx_new, dtype=np.int)
 
-            # 填充100*100像素点
-            # arr = np.zeros([500, 500], np.uint8)
+            # 填充256*256像素点
             arr = np.full((256, 256), 255, np.uint8)
             data_list = [tuple(x) for x in data_tmp]
 
@@ -162,7 +163,7 @@ if __name__ == '__main__':
 
             # 计算面积
             name = loc.replace('.csv', '_' + str(idx) + '.jpg')
-            s2 = img2area().countArea(name, arr)
+            s2 = Img2Area().count_area(name, arr)
             print('在原始数据中近似求解的面积为：' + str(s2))
 
             # 保存文件
