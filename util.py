eed82a236eadb500b80e90c43323e5f5439b42e6
ShiningShar <422620530@qq.com>
2020-12-03 09:51:47 +0800

    add img2area

--- util.py
+++ util.py
@@ -5,6 +5,9 @@ created on 2020/10/28 12:10
 """
 
 import math
+import os
+import random
+import cv2 as cv
 
 
 def match_data(x, f):
@@ -22,3 +25,5 @@ def zero_data(x, f):
 
 def nan_data(x):
     return isinstance(x, float) and math.isnan(x)
+
+
